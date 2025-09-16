0b777a7e9124a86c729963698503a27d1b096fa4
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-09-16 08:34:08 -0300

    feat: update LandingPage component

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -1,21 +1,8 @@
-import { useState } from "react";
-import { Modifications } from "../ui/Modifications";
-import { NotificationListener } from "../ui/NotificationListener";
 
 export default function LandingPage() {
-  const [showModifications, setShowModifications] = useState(false);
-
-  const handleShowModifications = () => {
-    setShowModifications(!showModifications);
-  }
-
   return (
     <main>
-      <NotificationListener />
-      <button className="bg-blue-600" onClick={handleShowModifications}>Alternar</button>
-      {
-        showModifications && <Modifications />
-      }
+      
     </main>
   );
 }
