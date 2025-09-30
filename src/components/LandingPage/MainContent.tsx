455437469f2edb6a37ef835ee1c881b7081b6f0d
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-09-29 21:39:22 -0300

    style: update spacing and margins

--- src/components/LandingPage/MainContent.tsx
+++ src/components/LandingPage/MainContent.tsx
@@ -3,11 +3,11 @@ import { CharacterImage } from "@/components/LandingPage/CharacterImage";
 
 export const MainContent = () => {
   return (
-    <section className="flex justify-between items-center main-padding">
+    <section className="screen-responsive-max-width main-padding flex justify-between items-center">
       <div className="flex flex-col gap-20">
-        <article className="flex flex-col gap-7">
+        <article className="flex flex-col gap-8">
           <div className="w-full">
-            <h1 className="font-light text-4xl mb-0">DESCUBRE MODS</h1>
+            <h1 className="font-light text-4xl mb-3">DESCUBRE MODS</h1>
             <h1 className="font-extrabold text-5xl">DE STALKER EN ESPAÑOL</h1>
           </div>
           <p className="font-normal max-w-[50ch]">
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,9 +3,11 @@ import { Navbar } from "./Navbar";
 
 export const Header = () => {
   return (
-    <header className="shadow-[0px_4px_40px_rgba(0,0,0,0.1)] flex flex-row gap-8 px-8 py-4">
-      <HeaderTitle />
-      <Navbar />
+    <header className="shadow-[0px_4px_40px_rgba(0,0,0,0.1)]">
+      <div className="screen-responsive-max-width flex flex-row gap-8 px-10 py-6">
+        <HeaderTitle />
+        <Navbar />
+      </div>
     </header>
   );
 };
