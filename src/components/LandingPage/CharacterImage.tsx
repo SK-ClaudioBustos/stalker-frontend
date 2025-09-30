4cbde948f729dedcd69fc500c9d7435d11e487ad
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-09-29 21:38:42 -0300

    refactor<CharacterImage>: update component style and update image ubication

--- src/components/LandingPage/CharacterImage.tsx
+++ src/components/LandingPage/CharacterImage.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
+import MercenaryCharacter from "./img/mercenary_character.webp";
 
 export const CharacterImage = () => {
   return (
-    <picture>
-      <Image
-        alt="mercenary character image"
-        width={300}
-        height={500}
-        src="/img/mercenary_character.webp"
-        style={{filter: "drop-shadow(0px 9px 5.5px rgba(0, 0, 0, 0.4))"}}
-      />
-    </picture>
+    <Image
+      alt="mercenary character image"
+      width={300}
+      height={500}
+      src={MercenaryCharacter}
+      style={{ filter: "drop-shadow(0px 9px 5.5px rgba(0, 0, 0, 0.4))" }}
+      className="xl:block hidden"
+    />
   );
 };
