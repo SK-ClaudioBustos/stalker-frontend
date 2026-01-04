bb1393865adfff3a7bf88b9dcb109ff7192e876a
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2026-01-04 18:16:48 -0300

    feat: remove old files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import { AppProvider } from "@/components/layout/AppProvider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ReactNode } from "react";
 import "./globals.css";
-import { Header } from "@/components/layout/Header";
 
 const fontFamily = Inter({
   subsets: ["latin"],
@@ -16,12 +16,11 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="es">
       <body className={fontFamily.className}>
-        <Header/>
         <AppProvider>{children}</AppProvider>
       </body>
     </html>
--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -1,13 +1,8 @@
-import { AvalaibleMods } from "@/components/LandingPage/AvalaibleMods/AvalaibleMods";
-import { MainContent } from "@/components/LandingPage/MainContent/MainContent";
-import { NewsSection } from "@/components/LandingPage/NewsSection/NewsSection";
 
 export default function LandingPage() {
   return (
     <main>
-      <MainContent />
-      <NewsSection />
-      <AvalaibleMods />
+      
     </main>
   );
 }
